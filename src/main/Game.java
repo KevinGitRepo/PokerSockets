0b24208f53999f9a3e312f1a9bda42cd97d0053d
vboxuser <ksakowicz10@gmail.com>
2025-01-21 18:31:04 +0000

    Main game loop comments and refactoring in game.java class

--- src/main/Game.java
+++ src/main/Game.java
@@ -8,102 +8,147 @@ class Game{
 
     private List<Card> dealerCards;
     private int pot;
+    private List<Player> players;
+    private Scanner scan;
 
+    // Main game function
     public void start(){
         this.dealerCards = new ArrayList<>();
-        Scanner scan = new Scanner(System.in);
-        List<Player> players = new ArrayList<Player>();
+        this.players = new ArrayList<Player>();
+        this.scan = new Scanner(System.in);
+
         Deck deck = new Deck();
 
         System.out.println("Hello.\nWelcome to Poker.\nPlease enter the amount of players that would like to play.");
-        int numOfPlayers = scan.nextInt();
+        int numOfPlayers = this.scan.nextInt();
         for (int i = 0; i < numOfPlayers; i++) {
-            players.add(new Player("Player " + (i + 1), 2000));
+            this.players.add(new Player("Player " + (i + 1), 2000));
         }
         while(true){
-            while(!players.isEmpty()){
-                System.out.println(players);
-                System.out.println("Enter the player number if player wants to leave (0 to continue).");
-                int playerNum = scan.nextInt();
-                scan.nextLine();
-                if(playerNum == 0){
-                    break;
-                }
-                if(playerNum < players.size()){
-                    players.remove(playerNum);
-                }
-            }
+            // Checks if any player wants to leave
+            dealWithLeavingPlayers();
 
-            deck.shuffle();
-
-            if(players.isEmpty()){
+            // Makes sure there are players in game
+            if(this.players.isEmpty()){
                 break;
             }
 
-            for(int i = 1 ; i < players.size() * 2 ; i++){
-                players.get(Math.abs(players.size() - i)).receiveCard(deck.dealCard());
-            }
-            players.getFirst().receiveCard(deck.dealCard());
+            // Shuffles the deck
+            deck.shuffle();
 
+            // Distribute cards to each player
+            for(int i = 1 ; i < this.players.size() * 2 ; i++){
+                this.players.get(Math.abs(this.players.size() - i)).receiveCard(deck.dealCard());
+            }
+            // Player in slot 0 needs to separately receive second card due to for loop
+            this.players.getFirst().receiveCard(deck.dealCard());
+
+            List<Player> remainingPlayers = new ArrayList<>(this.players);
+
+            // Main Game Loop
             while(true){
-                List<Player> remainingPlayers = new ArrayList<>(players);
+
+                List<Player> checkPlayers = new ArrayList<>();
+
                 int currentBet = 0;
-                for(Player player : players){
-                    if(!player.isAbleToPlay() && remainingPlayers.contains(player)){
+
+                // Round Game Loop
+                for(Player player : remainingPlayers){
+                    // Removes any player without sufficient balance to play
+                    if(!player.isAbleToPlay()){
                         remainingPlayers.remove(player);
                         continue;
                     }
                     System.out.println(player + "'s turn.\nYou have: " + player.getHand() + "\nPlease choose to fold (f), bet (b), or check (c).");
-                    String decision = scan.nextLine();
+                    String decision = this.scan.nextLine();
                     switch (decision) {
+                        // Fold case
                         case "f" -> {
                             player.fold();
                             remainingPlayers.remove(player);
                         }
+                        // Bet case
                         case "b" -> {
-                            System.out.println("Please enter the amount which you would like to bet. Current bet: " + currentBet + " Pot: " + pot);
-                            int bet = scan.nextInt();
-                            scan.nextLine();
+                            System.out.println("Please enter the amount which you would like to bet. Current balance: " + player.getMoneyLimit() + " Current bet: " + currentBet + " Pot: " + pot);
+                            int bet = this.scan.nextInt();
+                            this.scan.nextLine();
                             if (bet < player.getMoneyLimit() && bet >= currentBet) {
                                 currentBet = bet;
                                 player.bet(bet);
-                                pot += bet;
+                                this.pot += bet;
                             }
                         }
+                        // Check case
                         case "c" -> {
                             continue;
                         }
                     }
                 }
-                if(remainingPlayers.isEmpty()){
-                    reset();
-                    break;
-                }
-                if(remainingPlayers.size() == 1) {
-                    remainingPlayers.getFirst().win(pot);
-                    reset();
-                    break;
-                }
 
-                if(this.dealerCards.size() == 5){
-                    int indexOfWinner = 0;
-                    for(Player player : players){
-                        if(players.get(indexOfWinner).getPokerHand().amountWorth() < player.getPokerHand().amountWorth()){
-                            indexOfWinner = players.indexOf(player);
-                        }
-                    }
-                    players.get(indexOfWinner).win(pot);
-                    reset();
-                    break;
-                }
+                // Need to check if the game needs to be reset or if it can continue
+                if(checkForReset(remainingPlayers)){reset();break;}
 
-                dealerCards.add(deck.dealCard());
-                System.out.println("Dealer has: " + String.join(", ", dealerCards.toString()));
+                this.dealerCards.add(deck.dealCard());
+                System.out.println("Dealer has: " + String.join(", ", this.dealerCards.toString()));
             }
         }
         System.out.println("Thanks for playing!");
     }
 
+    /**
+     * Prompts the user for any person leaving and removes them from the players list
+     */
+    private void dealWithLeavingPlayers(){
+        while(!this.players.isEmpty()){
+            System.out.println(this.players);
+            System.out.println("Enter the player number if player wants to leave (0 to continue).");
+            int playerNum = this.scan.nextInt();
+            this.scan.nextLine();
+            if(playerNum == 0){
+                break;
+            }
+            if(playerNum < this.players.size()){
+                this.players.remove(playerNum);
+            }
+        }
+    }
+
+    /**
+     * Checks if the game needs a reset meaning that someone has won the game or no more players remain
+     * @param remainingPlayers list of all remaining players
+     * @return true if reset is required, false otherwise
+     */
+    private boolean checkForReset(List<Player> remainingPlayers){
+        // No players remain
+        if(remainingPlayers.isEmpty()){
+            return true;
+        }
+        // Only one person remains
+        else if(remainingPlayers.size() == 1) {
+            remainingPlayers.getFirst().win(this.pot);
+            System.out.println(remainingPlayers.getFirst() + " won!");
+            return true;
+        }
+        // The dealer has 5 cards and a winner needs to be decided
+        if(this.dealerCards.size() == 5){
+            int indexOfWinner = 0;
+            for(Player player : this.players){
+                // Compares each player's card worth amount to determine winner
+                if(this.players.get(indexOfWinner).getPokerHand().amountWorth() < player.getPokerHand().amountWorth()){
+                    indexOfWinner = this.players.indexOf(player);
+                }
+            }
+            this.players.get(indexOfWinner).win(this.pot);
+            System.out.println(this.players.get(indexOfWinner) + " won!");
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Resets the dealer and the pot for the next game to start
+     */
     public void reset(){
         this.dealerCards.clear();
         this.pot = 0;
