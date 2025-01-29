dc7d9ff0de66e5a5d6cbe6d785dd53eb25d6a383
vboxuser <ksakowicz10@gmail.com>
2025-01-28 22:56:27 -0500

    Final commit before introducing Maven dependencies

--- poker-sockets/src/App.js
+++ poker-sockets/src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import io from 'socket.io-client';
 
 function App() {
+  const [message, setMessage] = useState('');
+  const [socket, setSocket] = useState(null);
+
+  useEffect(() => {
+    const localSocket = io('http://localhost:5000');
+
+    localSocket.on('connect', () => {
+      console.log('Connect to WebSocket');
+    });
+
+    localSocket.on('message', (data) => {
+      setMessage(data);
+    });
+
+    setSocket(localSocket);
+
+    return () => {
+      localSocket.disconnect();
+    };
+  }, []);
+
+  const sendMessage = () => {
+    socket.emit('message', 'Hello from React!');
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Socket.IO Communication</h1>
+      <button onClick={sendMessage}>Send Message to Server</button>
+      <p>Message from server: {message}</p>
     </div>
   );
 }
---
+++ src/Sockets/PokerSocketServer.java
@@ -0,0 +1,8 @@
+//package Sockets;
+//import org.java-websocket.WebSocket;
+//import org.java-websocket.server.WebSocketServer;
+//import org.java-websocket.handshake.ClientHandshake;
+//import java.net.InetSocketAddress;
+//
+//public class PokerSocketServer {
+//}
