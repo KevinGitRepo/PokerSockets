968afef884028c1d21b70cce0d1c4fa2e716ed88
vboxuser <ksakowicz10@gmail.com>
2025-01-28 23:35:48 -0500

    Added spring boot format for websockets and will need to refactor to convert

---
+++ SpringSockets/src/main/java/org/example/springsockets/SpringSocketsApplication.java
@@ -0,0 +1,13 @@
+package org.example.springsockets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSocketsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSocketsApplication.class, args);
+    }
+
+}
---
+++ SpringSockets/src/test/java/org/example/springsockets/SpringSocketsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springsockets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSocketsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
